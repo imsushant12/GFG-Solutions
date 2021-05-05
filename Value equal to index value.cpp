fbb71d558a66b58d766631140ca9b55afa53cc1c
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-05-06 00:37:59 +0530

    first commit

---
+++ Value equal to index value.cpp
@@ -0,0 +1,50 @@
+/*
+Problem Statement:
+------------------
+Given an array Arr of N positive integers. Your task is to find the elements whose value is equal to that of its index value.
+
+Example 1:
+---------
+Input: 
+N = 5
+Arr[] = {15, 2, 45, 12, 7}
+Output: 2
+Explanation: Only Arr[2] = 2 exists here.
+
+Example 2:
+---------
+Input: 
+N = 1
+Arr[] = {1}
+Output: 1
+Explanation: Here Arr[1] = 1 exists.
+
+Your Task: You don't need to read input or print anything. Your task is to complete the function valueEqualToIndex() which takes the array of integers 
+arr[] and n as parameters and returns an array of indices where the given conditions are satified. When there is not such element exists then return an 
+empty array of length 0.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Note: There can be more than one element in the array which have same value as their index. You need to include every such element's index. 
+Follows 1-based indexing of the array.
+*/
+
+// Link --> https://practice.geeksforgeeks.org/problems/value-equal-to-index-value1330/1#
+// Code:
+class Solution{
+public:
+    vector <int> result;
+	vector<int> valueEqualToIndex(int a[], int n) 
+	{
+	    result.clear();
+	    
+	    for(int i=0 ; i<n ; i++)
+	    {
+	        if(a[i] == i+1)
+	            result.push_back(i+1);
+	    }
+	        
+	    return result;
+	}
+};
