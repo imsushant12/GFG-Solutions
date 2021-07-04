a234d65215c10995ccba49927686c524950c5414
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-07-04 23:21:47 +0530

    first commit

--- Minimum_Platforms.cpp
+++ Minimum_Platforms.cpp
@@ -1,6 +1,6 @@
 /*
 Problem Statement:
------------------
+------------------
 Given arrival and departure times of all trains that reach a railway station. Find the minimum number of platforms required for the railway station so that no train 
 is kept waiting. Consider that all the trains arrive on the same day and leave on the same day. Arrival and departure time can never be the same for a train 
 but we can have arrival time of one train equal to departure time of the other. At any given instance of time, same platform can not be used for 
