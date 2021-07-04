02da430ff868d0c82466432f36357f625ae346c1
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-07-04 23:23:13 +0530

    first commit

--- Choose_and_Swap.cpp
+++ Choose_and_Swap.cpp
@@ -1,6 +1,6 @@
 /*
 Problem Statement:
-------------------
+-----------------
 You are given a string s of lower case english alphabets. You can choose any two characters in the string and replace all the occurences of the first character 
 with the second character and replace all the occurences of the second character with the first character. 
 Your aim is to find the lexicographically smallest string that can be obtained by doing this operation at most once.
