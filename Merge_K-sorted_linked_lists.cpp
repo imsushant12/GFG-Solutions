8aed3de8575715d6a4344d7ad1ce1ae349e85221
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-07-04 23:21:56 +0530

    first commit

--- Merge_K-sorted_linked_lists.cpp
+++ Merge_K-sorted_linked_lists.cpp
@@ -1,6 +1,6 @@
 /*
 Problem Statement:
-------------------
+-----------------
 Given K sorted linked lists of different sizes. The task is to merge them in such a way that after merging they will be a single sorted linked list.
 
 Example 1:
