858ed1fe203810af58f11905277cada34a2ef1df
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-07-04 23:22:13 +0530

    first commit

--- Maximize_sum-[arr[i]*i]_of_an_Array.cpp
+++ Maximize_sum-[arr[i]*i]_of_an_Array.cpp
@@ -1,6 +1,6 @@
 /*
 Problem Statement:
-------------------
+-----------------
 Given an array A of N integers. Your task is to write a program to find the maximum value of ∑arr[i]*i, where i = 0, 1, 2,…., n – 1.
 You are allowed to rearrange the elements of the array. Note: Since output could be large, hence module 109+7 and then print answer.
 
