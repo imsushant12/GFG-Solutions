b620f255004d1718ce168a2dea4159c1592cd280
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-07-04 23:23:03 +0530

    first commit

--- Clone_a_linked_list_with_next_and_random_pointer.cpp
+++ Clone_a_linked_list_with_next_and_random_pointer.cpp
@@ -1,6 +1,6 @@
 /*
 Problem Statement:
-------------------
+-----------------
 You are given a special linked list with N nodes where each node has a next pointer pointing to its next node. You are also given M random pointers , 
 where you will be given M number of pairs denoting two nodes a and b  i.e. a->arb = b.
 
