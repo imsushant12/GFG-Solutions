9bf5155953efb8fa010130862f15e6ee29f81aed
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-07-04 23:22:44 +0530

    first commit

--- First_non-repeating_character_in_a_stream.cpp
+++ First_non-repeating_character_in_a_stream.cpp
@@ -1,6 +1,6 @@
 /*
 Problem Statement:
-------------------
+-----------------
 Given an input stream of A of n characters consisting only of lower case alphabets. The task is to find the first non repeating character, 
 each time a character is inserted to the stream. If there is no such character then append '#' to the answer.
  
