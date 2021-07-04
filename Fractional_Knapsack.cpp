70c1c7fed799889ff686853902bb0aea7fb9b243
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-07-04 23:22:34 +0530

    first commit

--- Fractional_Knapsack.cpp
+++ Fractional_Knapsack.cpp
@@ -1,6 +1,6 @@
 /*
 Problem Statement:
-------------------
+-----------------
 Given weights and values of N items, we need to put these items in a knapsack of capacity W to get the maximum total value in the knapsack.
 Note: Unlike 0/1 knapsack, you are allowed to break the item. 
 
